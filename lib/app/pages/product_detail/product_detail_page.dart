078713c4b2da6027adc0b944aaa4433050e1019a
Mario Moraes <moraes_mario@hotmail.com>
2023-02-01 16:42:32 -0300

    Changes

--- lib/app/pages/product_detail/product_detail_page.dart
+++ lib/app/pages/product_detail/product_detail_page.dart
@@ -1,4 +1,7 @@
+import 'package:dw9_vakinha_burger_bloc/app/core/ui/helpers/size_extensions.dart';
 import 'package:flutter/material.dart';
+
+import '../../core/ui/widgets/delivery_app_bar.dart';
 
 class ProductDetailPage extends StatelessWidget {
   const ProductDetailPage({Key? key}) : super(key: key);
@@ -6,10 +9,18 @@ class ProductDetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Product Detail'),
-      ),
-      body: Container(),
+      appBar: DeliveryAppBar(),
+      body: Column(children: [
+        Container(
+          width: context.width,
+          height: context.percentHeight(.4),
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: NetworkImage(
+                    'https://assets.unileversolutions.com/recipes-v2/106684.jpg?imwidth=800')),
+          ),
+        ),
+      ]),
     );
   }
 }
